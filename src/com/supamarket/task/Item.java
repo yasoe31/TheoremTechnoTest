e9e7bc16b32abffdccc3833865f778ee324e00b0
yasoe31 <yasoe31@gmail.com>
2015-08-04 22:26:45 +0100

    Item Object Created

---
+++ src/com/supamarket/task/Item.java
@@ -0,0 +1,28 @@
+package com.supamarket.task;
+
+public class Item {
+	
+	private String name;
+	private int quantity;
+	private float value;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public float getValue() {
+		return value;
+	}
+	public void setValue(float value) {
+		this.value = value;
+	}	
+
+}
